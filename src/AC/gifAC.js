0a5e0ce6aaee95585bfae536bb2a791766e6fbae
Valeriy Saphonov <xxrrrxx@yandex.ru>
2018-05-08 09:49:26 +0300

    add-axios+async/await-example

--- src/AC/gifAC.js
+++ src/AC/gifAC.js
@@ -1,11 +1,20 @@
 import { FETCH_DATA_REQUEST, API_URL } from "../constants";
-import fetch from "isomorphic-fetch";
+// import fetch from "isomorphic-fetch";
+import axios from "axios";
 
-export const refreshGif = () => (dispatch) => {
-  fetch(API_URL).then(response => response.json()).then(images =>
-    dispatch({
-      type: FETCH_DATA_REQUEST,
-      payload: images.data.image_original_url
-    })
-  );
+// export const refreshGif = () => (dispatch) => {
+//   fetch(API_URL).then(response => response.json()).then(images =>
+//     dispatch({
+//       type: FETCH_DATA_REQUEST,
+//       payload: images.data.image_original_url
+//     })
+//   );
+// };
+
+export const refreshGif = () => async dispatch => {
+  const images = await axios.get(API_URL);
+  dispatch({
+    type: FETCH_DATA_REQUEST,
+    payload: images.data.data.image_original_url
+  });
 };
